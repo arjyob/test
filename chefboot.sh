c1543da888aa708b7bc99c5abf89405df29fb1d3
Arjyo <arjyob@yahoo.com>
2015-06-08 23:46:38 +0530

    Update

--- chefboot.sh
+++ chefboot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws ec2 describe-network-interfaces|grep -i ASSOCIATION |awk '{print $4}'|sort -u > /tmp/ec2ip
+/home/arjyob/.local/lib/aws/bin/aws ec2 describe-network-interfaces|grep -i ASSOCIATION |awk '{print $4}'|sort -u > /tmp/ec2ip
 for i in `cat /tmp/ec2ip`
 do
 /usr/bin/knife bootstrap $i
