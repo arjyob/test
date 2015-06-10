e8487879e8adae30649a47ed0306ccfd3f85cc31
arjyob <arjyob@yahoo.com>
2015-06-10 16:35:54 +0530

    Update chefboot.sh

--- chefboot.sh
+++ chefboot.sh
@@ -2,5 +2,5 @@
 /usr/bin/aws ec2 describe-network-interfaces|grep -i ASSOCIATION |awk '{print $4}'|sort -u > /tmp/ec2ip
 for i in `cat /tmp/ec2ip`
 do
-/usr/bin/knife bootstrap $i
+/opt/chefdk/embedded/bin/knife bootstrap $i --ssh-user ec2-user -i /home/ec2-user/CHEF_PAAS_DEV_KEYS.pem -N test1 --sudo
 done
